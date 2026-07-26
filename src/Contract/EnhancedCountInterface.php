34429bed5b9a51ca600ecf186c71cbb6b22b8ad4
Romain UGOLINI <rugolinifr@gmail.com>
2026-07-26 22:08:56 +0200

    efb-024: adds contract for COUNT()

---
+++ src/Contract/EnhancedCountInterface.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rugolinifr\EnhancedFindBy\Contract;
+
+interface EnhancedCountInterface
+{
+    /**
+     * @template T of object
+     * Type `T` is any entity type.
+     *
+     * Counts the number of entities persisted in a database and matching the given parameters.
+     * The parameters mirror a regular DQL query.
+     *
+     * The following example counts humans whose first name starts with "Mary-",
+     * born before the third millennium and working for 'The Deep Bottle' company.
+     * The count is limited to a maximum of 30 entities, starting after the 60th.
+     *
+     * <code>
+     * $count = $finder->count(
+     *     from: Person::class,
+     *     where: [
+     *         'firstName like' => 'Mary-%',
+     *         'birth <' => new DateTimeImmutable('2000-01-01'),
+     *         'currentJob.company.name =' => 'The Deep Bottle', //Two implicit INNER JOINs are made here
+     *     ],
+     *     limit: 30,
+     *     offset: 60,
+     * );
+     * </code>
+     *
+     * @param class-string<T> $from the class name of the entities to count,
+     * retrieved from either the `get_class()` function or the `::class` keyword.
+     * @param array<string, mixed> $where an ordered map where keys are property paths followed by an operator,
+     *  mapped to the value to compare against.
+     * @param int|null $limit the maximum number of entities to count, greater than 0, or `null` to count every matching entity.
+     * @param int $offset the number of entities to skip before starting to count the result set,
+     * greater than or equal to 0.
+     * This parameter is ignored when `$limit` is `null`.
+     * @return int the number of matching entities.
+     *
+     * @throws EnhancedFindByInvalidArgumentException when any given argument is either invalid or forbidden.
+     * @throws EnhancedCountExceptionInterface when any error occurs.
+     */
+    public function count(
+        string $from,
+        array $where = [],
+        ?int $limit = null,
+        int $offset = 0,
+    ): int;
+}
