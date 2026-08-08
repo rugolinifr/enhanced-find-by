f5a2ebb86158f39e35ebba53d3fab119f9386bfb
Romain UGOLINI <rugolinifr@gmail.com>
2026-08-08 13:29:59 +0200

    efb-025: melt the two implementations into one

--- src/Factory/EnhancedFindByFactory.php
+++ src/Factory/EnhancedFindByFactory.php
@@ -7,8 +7,7 @@ namespace Rugolinifr\EnhancedFindBy\Factory;
 use Doctrine\ORM\EntityManagerInterface;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedCountInterface;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedFindByInterface;
-use Rugolinifr\EnhancedFindBy\Implementation\Finder\EnhancedCount;
-use Rugolinifr\EnhancedFindBy\Implementation\Finder\EnhancedFindBy;
+use Rugolinifr\EnhancedFindBy\Implementation\Finder\EnhancedImplementation;
 use Rugolinifr\EnhancedFindBy\Implementation\QueryBuilder\ComparisonFactory;
 use Rugolinifr\EnhancedFindBy\Implementation\QueryBuilder\QueryBuilder;
 use Rugolinifr\EnhancedFindBy\Implementation\Shared\AliasedPropertyProvider;
@@ -16,29 +15,41 @@ use Rugolinifr\EnhancedFindBy\Implementation\Shared\JoinClauseProvider;
 
 class EnhancedFindByFactory
 {
-    private ?QueryBuilder $queryBuilder = null;
+    private ?EnhancedImplementation $implementation = null;
 
-    public function createEnhancedFindBy(EntityManagerInterface $entityManager): EnhancedFindByInterface
-    {
-        if ($this->queryBuilder === null) {
-            $this->queryBuilder = $this->createQueryBuilder($entityManager);
-        }
-        return new EnhancedFindBy($this->queryBuilder);
+    public function createEnhancedFindBy(
+        EntityManagerInterface $entityManager,
+        bool $fromCache = true,
+    ): EnhancedFindByInterface {
+        return $this->createEnhancedImplementation($entityManager, $fromCache);
     }
 
-    public function createEnhancedCount(EntityManagerInterface $entityManager): EnhancedCountInterface
-    {
-        if ($this->queryBuilder === null) {
-            $this->queryBuilder = $this->createQueryBuilder($entityManager);
-        }
-        return new EnhancedCount($this->queryBuilder);
+    public function createEnhancedCount(
+        EntityManagerInterface $entityManager,
+        bool $fromCache = true,
+    ): EnhancedCountInterface {
+        return $this->createEnhancedImplementation($entityManager, $fromCache);
     }
 
-    private function createQueryBuilder(EntityManagerInterface $entityManager): QueryBuilder
+    private function createEnhancedImplementation(
+        EntityManagerInterface $entityManager,
+        bool $fromCache = true,
+    ): EnhancedImplementation {
+        if ($this->implementation === null) {
+            $this->implementation = $this->createImplementation($entityManager);
+        }
+        if ($fromCache) {
+            return $this->implementation;
+        }
+        return $this->createImplementation($entityManager);
+    }
+
+    private function createImplementation(EntityManagerInterface $entityManager): EnhancedImplementation
     {
         $joinClauseProvider = new JoinClauseProvider();
         $aliasedPropertyProvider = new AliasedPropertyProvider();
         $comparisonFactory = new ComparisonFactory($entityManager, $joinClauseProvider, $aliasedPropertyProvider);
-        return new QueryBuilder($comparisonFactory, $entityManager);
+        $queryBuilder = new QueryBuilder($comparisonFactory, $entityManager);
+        return new EnhancedImplementation($queryBuilder);
     }
 }
--- src/Implementation/Finder/EnhancedImplementation.php
+++ src/Implementation/Finder/EnhancedImplementation.php
@@ -7,15 +7,34 @@ namespace Rugolinifr\EnhancedFindBy\Implementation\Finder;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedCountInterface;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedCountInvalidArgumentException;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedFindByExceptionInterface;
+use Rugolinifr\EnhancedFindBy\Contract\EnhancedFindByInterface;
 use Rugolinifr\EnhancedFindBy\Contract\EnhancedFindByInvalidArgumentException as EFBInvalidArgumentException;
 use Rugolinifr\EnhancedFindBy\Implementation\QueryBuilder\QueryBuilder;
 use Rugolinifr\EnhancedFindBy\Implementation\QueryBuilder\QueryTypeEnum;
 
-class EnhancedCount implements EnhancedCountInterface
+class EnhancedImplementation implements EnhancedFindByInterface, EnhancedCountInterface
 {
+
     public function __construct(
         private QueryBuilder $queryBuilder,
     ) {
+    }
+
+    public function findBy(
+        string $from,
+        array $where = [],
+        array $orderBy = [],
+        ?int $limit = null,
+        int $offset = 0,
+    ): array {
+        return $this->queryBuilder->buildThenExecuteQuery( //@phpstan-ignore return.type
+            QueryTypeEnum::SELECT,
+            $from,
+            $where,
+            $orderBy,
+            $limit,
+            $offset,
+        );
     }
 
     public function count(
